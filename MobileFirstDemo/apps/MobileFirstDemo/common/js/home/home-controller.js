41385139ae20a07e477e87fbc1753009bbb197fb
ajgprieto <aj.garidanprieto@gmail.com>
2015-11-19 08:55:09 -1000

    list countries

--- MobileFirstDemo/apps/MobileFirstDemo/common/js/home/home-controller.js
+++ MobileFirstDemo/apps/MobileFirstDemo/common/js/home/home-controller.js
@@ -4,8 +4,21 @@
 		$scope.search = {};
 		
 		$scope.getAllCountries = function() {
-			alert($scope.search.field);
+			var invocationData = {
+					adapter : 'DemoAdapter', 
+					procedure : 'getAllCountries'
+			};
 			
+			WL.Client.invokeProcedure(invocationData, {
+				onSuccess : function(result) {
+					var countries = result.invocationResult.RestResponse.result;
+					$scope.countries = countries;
+					$scope.$apply();
+				},
+				onFailure : function() {
+					alert("something went wrong.  Blame Andrew");
+				}
+			})
 			
 		}
 	}
