a5efbb77e366c72276cc991a4ec2d59f130e7d2c
ajgprieto <aj.garidanprieto@gmail.com>
2015-11-19 16:12:44 -1000

    add search country

--- MobileFirstDemo/adapters/DemoAdapter/DemoAdapter-impl.js
+++ MobileFirstDemo/adapters/DemoAdapter/DemoAdapter-impl.js
@@ -8,3 +8,14 @@ function getAllCountries() {
 	
 	return WL.Server.invokeHttp(input);
 }
+
+function search2charCountryCode(code) {
+	
+	var input = {
+			method : 'get',
+			returnedContentType : 'json',
+			path : '/country/get/iso2code/' + code
+	};
+	
+	return WL.Server.invokeHttp(input);
+}
--- MobileFirstDemo/apps/MobileFirstDemo/common/js/home/home-controller.js
+++ MobileFirstDemo/apps/MobileFirstDemo/common/js/home/home-controller.js
@@ -19,6 +19,32 @@
 					alert("something went wrong.  Blame Andrew");
 				}
 			})
+		};
+		
+		$scope.searchByCountryCode2 = function() {
+			
+			var invocationData = {
+					adapter : 'DemoAdapter',
+					procedure : 'search2charCountryCode',
+					parameters : [$scope.search.countryCode]
+			};
+			
+			WL.Client.invokeProcedure(invocationData, {
+				onSuccess : function(result) {
+					var message = result.invocationResult.RestResponse.messages.toString();
+					if (message.toString().indexOf("requested") != -1) {
+						message = message.slice(message.indexOf(',') + 1);
+						$scope.result = message;
+					}
+					else {
+						$scope.result = result.invocationResult.RestResponse.result.name;
+					}
+					$scope.$apply();
+				},
+				onFailure : function() {
+					alert("something went wrong.  Don't blame Andrew");
+				}
+			});
 			
 		}
 	}
